99c755114ffe833ae2905d0eb1c8f893aa114581
VovaPleshchuk <90177843+VovaPleshchuk@users.noreply.github.com>
2024-05-31 13:09:25 +0300

    Delete 22 directory

--- 22/19.2.py
+++
@@ -1,9 +0,0 @@
-import random
-
-random_numbers = [random.randint(1, 10) for _ in range(10)]
-
-print("Згенеровані числа:", random_numbers)
-
-average = sum(random_numbers) / len(random_numbers)
-
-print("Середнє арифметичне:", average)
